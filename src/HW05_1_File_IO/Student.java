76c2cb2cb26addb7189846b32e97131619bfec2a
daone <jcn07253@gmail.com>
2023-10-09 19:24:48 +0900

    Homework 5.1 (주석문 추가) class Person, class Student, class StudentArray, sorting, file I/O

--- src/HW05_1_File_IO/Student.java
+++ src/HW05_1_File_IO/Student.java
@@ -1,31 +1,33 @@
 package HW05_1_File_IO;
 
+/* 학생 정보 관리를 위한 클래스 */
 public class Student extends Person {
-    private final int st_id;
-    private final String dept;
-    private final String school;
-    private final double GPA;
+    private final String school; // 학교
+    private final String dept; // 학과
+    private final int st_id; // 학번
+    private final double GPA; // 학점
 
-    public Student(String nm, int reg_id, int st_id, String dept, String school, double gpa) {
-        super(nm, reg_id);
-        this.st_id = st_id;
-        this.dept = dept;
+    // 생성자
+    public Student(String nm, int reg_id, String school, String dept, int st_id, double gpa) {
+        super(nm, reg_id); // 상위 클래스인 Person 의 생성자를 호출하여 이름과 등록 id 설정
         this.school = school;
+        this.dept = dept;
+        this.st_id = st_id;
         this.GPA = gpa;
     }
 
     @Override
-    public String toString() {
+    public String toString() { // 문자열로 객체 표현
         return String.format("Student(name: %5s, reg_id: %5d, school: %5s, dept: %5s, st_id: %5d, GPA: %5.2f)",
                 getName(), getRegID(), school, dept, st_id, GPA);
     }
 
-    public int compareStudent(String key_attr, Student other) {
+    public int compareStudent(String key_attr, Student other) { // 정렬할 때 어떤 속성으로 정렬할지
         return switch (key_attr) {
-            case "name" -> this.getName().compareTo(other.getName());
-            case "st_id" -> Integer.compare(this.st_id, other.st_id);
-            case "GPA" -> Double.compare(this.GPA, other.GPA);
-            default -> 0;
+            case "name" -> this.getName().compareTo(other.getName()); // 이름 순
+            case "st_id" -> Integer.compare(this.st_id, other.st_id); // 학번 순
+            case "GPA" -> Double.compare(this.GPA, other.GPA); // 학점 순
+            default -> 0; // 기본 값
         };
     }
 }
--- src/HW05_1_File_IO/StudentArray.java
+++ src/HW05_1_File_IO/StudentArray.java
@@ -3,67 +3,68 @@ package HW05_1_File_IO;
 import java.io.*;
 import java.util.Scanner;
 
+/* Student 의 인스턴스들을 배열로 관리하는 클래스 */
 public class StudentArray {
-    private Student[] students;
+
+    // 데이터 멤버
+    private Student[] students; // Student 객체 배열
     private int num_students;
 
+    // 생성자
     public StudentArray() {
-        students = new Student[10];
+        students = new Student[5]; // 총 학생이 5명이기 때문에 5 크기의 배열을 생성
         num_students = 0;
     }
 
+    /* 입력 파일의 학생 정보를 읽고 배열에 저장하는 메서드 */
     public void fget_students(Scanner fin) {
         while (fin.hasNextLine()) {
-            String line = fin.nextLine();
-            String[] parts = line.split(" ");
-
-            if (parts.length != 6) {
+            String line = fin.nextLine(); // 한줄 씩 읽음
+            String[] parts = line.split(" "); // 공백 문자로 분할하여 문자열 배열에 저장
+            if (parts.length != 6) { // 학생 정보는 총 6가지의 정보를 담고 있음, 이게 아니라면 다음 줄을 읽도록 함
                 System.out.println("Invalid data format: " + line);
                 continue;
             }
-
-            String name = parts[0];
+            String name = parts[0]; // 한 줄에서 학생의 정보를 추출함
             int reg_id = Integer.parseInt(parts[1]);
             int st_id = Integer.parseInt(parts[2]);
             String dept = parts[3];
             String school = parts[4];
             double gpa = Double.parseDouble(parts[5]);
 
-            students[num_students] = new Student(name, reg_id, st_id, dept, school, gpa);
-            num_students++;
+            students[num_students] = new Student(name, reg_id, school, dept, st_id, gpa); // 객체 생성 및 student 배열에 추가
+            num_students++; // 총 학생 수 체크
         }
     }
 
-
+    /* 배열에 저장된 학생의 정보를 콘솔에 출력하는 메서드 */
     public void print_students() {
-        System.out.println("Total " + num_students + " students :");
+        System.out.println("Total " + num_students + " students :"); // 총 학생 수 출력
         for (int i = 0; i < num_students; i++) {
             System.out.println(students[i]);
         }
     }
 
-
+    /* 배열에 저장된 학생의 정보를 파일에 출력하는 메서드 */
     public void fprint_students(FileWriter fout) throws IOException {
-        fout.write("Total " + num_students + " students :\n");
+        fout.write("Total " + num_students + " students :\n"); // 총 학생 수 출력
         fout.flush();
         for (int i = 0; i < num_students; i++) {
             fout.write(students[i].toString() + "\n");
         }
     }
 
-
+    /* 학생 정보를 속성에 따라서 정렬하는 메서드 */
     public void sort(String key_attr, String sorting_order) {
-        for (int i = 1; i < num_students; i++) {
+        for (int i = 1; i < num_students; i++) { // 삽입 정렬 알고리즘
             Student currentStudent = students[i];
             int j = i - 1;
-
             while (j >= 0 && (sorting_order.equals("increasing")
-                    ? students[j].compareStudent(key_attr, currentStudent) > 0
-                    : students[j].compareStudent(key_attr, currentStudent) < 0)) {
+                    ? students[j].compareStudent(key_attr, currentStudent) > 0 // sorting order 가 increasing 일 때 실행
+                    : students[j].compareStudent(key_attr, currentStudent) < 0)) { // sorting order 가 decreasing 일 때 실행
                 students[j + 1] = students[j];
                 j--;
             }
-
             students[j + 1] = currentStudent;
         }
     }
